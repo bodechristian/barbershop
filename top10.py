31c56c5832546d96ba6e770a183a9cff5fddce8f
Christian Bode <christianbode97@web.de>
2026-07-05 10:33:48 +0200

    added chamionship winners and start/end year

--- top10.py
+++ top10.py
@@ -1,15 +1,20 @@
 from pathlib import Path
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 
+MIN_FINISHES = 7
+PATH_FOLDER = Path("top10scores")
 
-folder = Path("top10scores")
+YEAR_START = 1970
+YEAR_END = 2026
 
 records = []
 
-for file in sorted(folder.glob("*.txt")):
+for file in sorted(PATH_FOLDER.glob("*.txt")):
     year = int(file.stem)
+
+    if not (YEAR_START <= year <= YEAR_END):
+        continue
 
     df = pd.read_csv(file, sep="\t", encoding="utf-8-sig")
     df.columns = df.columns.str.strip()
@@ -26,27 +31,25 @@ for file in sorted(folder.glob("*.txt")):
 
 results = pd.DataFrame(records)
 
-# Sort chronologically
+## Get Quartets to display
 results = results.sort_values("year")
-
-# cumulative top-10 count per quartet
 results["top10s"] = results.groupby("quartet").cumcount() + 1
-
-# final counts per quartet
 final_counts = (
     results.groupby("quartet")["top10s"]
     .max()
     .sort_values(ascending=False)
 )
-
-min_finishes = 7
-keep_quartets = final_counts[final_counts >= min_finishes].index
-
+keep_quartets = final_counts[final_counts >= MIN_FINISHES].index
 results = results[results["quartet"].isin(keep_quartets)]
+
+## Get Winners for event markers
+last_rows = results.sort_values("year").groupby("quartet").tail(1)
+winners = last_rows[last_rows["place"] == 1]
 
 # ---- PLOTLY ----
 fig = go.Figure()
 
+## Display quartets
 # iterate in sorted order (IMPORTANT for legend order)
 for quartet in final_counts.loc[keep_quartets].index:
     g = results[results["quartet"] == quartet]
@@ -64,7 +67,28 @@ for quartet in final_counts.loc[keep_quartets].index:
         line_shape="hv"
     ))
 
+## Event markers for winning years
+fig.add_trace(go.Scatter(
+    x=winners["year"],
+    y=winners["top10s"],
+    mode="markers",
+    name="Championship finish",
+    marker=dict(
+        symbol="star",
+        size=14,
+        color="gold",
+        line=dict(width=1, color="black")
+    ),
+    hovertemplate=(
+        "🏆 %{customdata}<br>"
+        "Year: %{x}<br>"
+        "Final Top 10 #: %{y}<extra></extra>"
+    ),
+    customdata=winners["quartet"]
+))
+
 fig.update_layout(
+    title=f"Quartets with at least {MIN_FINISHES} top10 finishes",
     xaxis_title="Year",
     yaxis_title="Career Top 10 finishes",
     legend_title="Quartet (Top 10s)",
@@ -73,5 +97,6 @@ fig.update_layout(
 fig.update_xaxes(
     range=[results["year"].min() - 1, results["year"].max() + 1]
 )
+fig.update_traces(line=dict(width=2))
 
 fig.show()
